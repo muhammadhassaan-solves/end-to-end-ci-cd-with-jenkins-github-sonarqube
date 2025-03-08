2681b41e281efbbd9e646f0ba3ad7823965edc42
muhammadhassaan-solves <hassaan.solves@gmail.com>
2025-03-08 16:31:42 +0500

    Update MainApp.java

--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -4,5 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello, CI/CD Pipeline with Jenkins and MLflow!");
         System.out.println("Hello, CI/CD Pipeline with Jenkins and MLflow!");
+        System.out.println("Hello, CI/CD Pipeline with Jenkins and MLflow!");
+        
     }
 }
