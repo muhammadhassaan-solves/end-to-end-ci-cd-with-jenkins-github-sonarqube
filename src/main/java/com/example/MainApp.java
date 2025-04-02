19718bb8ab4288cf7a16fe7303293fd3313d111d
Muhammad Hassaan <hassaan.solves@gmail.com>
2025-04-03 03:19:27 +0500

    Update MainApp.java

--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class MainApp {
+    private static final String PASSWORD = "12345"; // Hardcoded password (Security issue)
+
     public static void main(String[] args) {
         System.out.println("Hello, This is end-to-end-ci-cd-with-jenkins-github-sonarqube!");
+
+        int unusedVar = 42; // Unused variable (Code smell)
+        int result = 10 / 0; // Division by zero (Bug)
     }
 }
